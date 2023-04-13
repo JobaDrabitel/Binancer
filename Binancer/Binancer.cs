dc74611eee0db0e018d9a87f8653b9350b8eef1b
unknown <dimentor@loh.true>
2023-04-13 12:12:14 +0300

    'First-vrode-norm-commit'

--- Binancer/Binancer.cs
+++ Binancer/Binancer.cs
@@ -6,36 +6,39 @@ namespace Binancer
 
     class Binancer
     {
+        static BinanceSocketClient? socketClient;
         static readonly string trx = "trxusdt";
         static readonly string[] symbols = new string[] { "btcusdt", "ethusdt", "dogeusdt" };
-        static BinanceSocketClient? socketClient;
         static decimal bidForBuy = 0m;
         static decimal askForSell = 0m;
         static decimal myQuantity = 0.001m;
         static async Task Main(string[] args)
         {     
-            var apiCredentials = GetApiCredentials();
+            var apiCredentials = GetBinanceInfo.GetApiCredentials();
             socketClient = GetBinanceSocketClient(apiCredentials);
 
             await GetCryptoDataAsync(trx);
-            while (true) { }
+
+            Console.ReadLine();
         }
         static async Task GetCryptoDataAsync(string symbol)
         {
-            if (socketClient == null) { return; }
+            if (socketClient == null) 
+                return;
+
             var depth = await socketClient.SpotStreams.SubscribeToBookTickerUpdatesAsync(symbol, async data =>
             {
                 if (data.Data.BestBidPrice < bidForBuy)
-                    await BuyAsync(trx, myQuantity, data.Data.BestBidPrice);
+                    await SendOrderAsync(trx, myQuantity, data.Data.BestBidPrice, OrderSide.Buy);
                 if (data.Data.BestAskPrice < askForSell)
-                    await SellAsync(trx, myQuantity, data.Data.BestAskPrice);
+                    await SendOrderAsync(trx, myQuantity, data.Data.BestAskPrice, OrderSide.Sell);
             });
+
+            await GetAccountInfo(GetBinanceInfo.GetBinanceClient(GetBinanceInfo.GetApiCredentials()));
         }
 
         static async Task GetAccountInfo(BinanceClient client)
         {
-           
-
             var accountInfo = await client.SpotApi.Account.GetAccountInfoAsync();
 
             if (accountInfo.Success)
@@ -52,8 +55,8 @@ namespace Binancer
         }
         static async Task PlaceOrderAsync(string mySymbol, OrderSide requiredSide, decimal requiredQuantity, decimal requiredPrice)
         {
-            var apiCredentials = GetApiCredentials();
-            var client = GetBinanceClient(apiCredentials);
+            var apiCredentials = GetBinanceInfo.GetApiCredentials();
+            var client = GetBinanceInfo.GetBinanceClient(apiCredentials);
 
             var result = await client.UsdFuturesApi.Trading.PlaceOrderAsync
             (
@@ -78,56 +81,15 @@ namespace Binancer
             }
         }
 
-        static async Task BuyAsync(string symbol, decimal quantity, decimal price)
-        {
-            await PlaceOrderAsync(symbol, OrderSide.Buy, quantity, price);
-        }
+        static async Task SendOrderAsync(string symbol, decimal quantity, decimal price, OrderSide side) => await PlaceOrderAsync(symbol, side, quantity, price);
 
-        static async Task SellAsync(string symbol, decimal quantity, decimal price)
-        {
-            await PlaceOrderAsync(symbol, OrderSide.Sell, quantity, price);
-        }
         static BinanceSocketClient GetBinanceSocketClient(BinanceApiCredentials apiCredentials)
         {
             var client = new BinanceSocketClient(new BinanceSocketClientOptions()
             {
                 ApiCredentials = apiCredentials
-
-            });
-            return client;
-        }
-        static BinanceApiCredentials GetApiCredentials()
-        {
-            var apiCredentials = new BinanceApiCredentials("<API key>", "<secret key>");
-            return apiCredentials;
-        }
-        static BinanceClient GetBinanceClient(BinanceApiCredentials apiCredentials)
-        {
-            var client = new BinanceClient(new BinanceClientOptions()
-            {
-                ApiCredentials = apiCredentials,
             });
             return client;
         }
     }
-
 }
-
-
-
-
-
-
-//        //if (data.Data.BestBidPrice < prevPrice)
-//        //{
-//        //    Console.WriteLine($"Price dropped below {prevPrice}: {data.Data.BestBidPrice}");
-//        //    prevPrice = data.Data.BestBidPrice;
-//        //}
-//        //if (data.Data.BestBidPrice > prevPrice)
-//        //{
-//        //    Console.WriteLine($"Price go up: {prevPrice}: {data.Data.BestBidPrice}");
-//        //    prevPrice = data.Data.BestBidPrice;
-//        //}
-
-//    Console.ReadLine();
-
---
+++ Binancer/Stock Markets/Binance/GetBinanceInfo.cs
@@ -0,0 +1,25 @@
+﻿using Binance.Net.Clients;
+using Binance.Net.Objects;
+
+namespace Binancer
+{
+    internal class GetBinanceInfo
+    {
+        public static BinanceApiCredentials GetApiCredentials()
+        {
+            var apiCredentials = new BinanceApiCredentials("<API key>", "<secret key>");
+
+            return apiCredentials;
+        }
+
+        public static BinanceClient GetBinanceClient(BinanceApiCredentials apiCredentials)
+        {
+            var client = new BinanceClient(new BinanceClientOptions()
+            {
+                ApiCredentials = apiCredentials,
+            });
+
+            return client;
+        }
+    }
+}
